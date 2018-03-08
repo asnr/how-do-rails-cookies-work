520bc9238f059f35e002d606e2251dca18bbdb55
Andres Sarnari <asarnari@Andress-MacBook-Pro.local>
2018-03-08 11:23:24 +1100

    Rename actions to follow rails conventions more closely

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,17 +1,17 @@
 class CookiesController < ApplicationController
-  def set_cookies
+  def create
     cookies[:plain] = 'Some unencrypted value'.freeze
     cookies.signed[:signed] = 'Some signed value'.freeze
     cookies.encrypted[:encrypted] = 'Some encrypted value'.freeze
     render html: "Consider your cookies set.\n".freeze
   end
 
-  def reset_cookies
+  def reset
     delete_all_cookies
     redirect_to '/set-cookies'
   end
 
-  def delete_cookies
+  def destroy
     delete_all_cookies
     render html: "Consider your cookies deleted.\n".freeze
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/set-cookies', to: 'cookies#set_cookies'
-  get '/reset-cookies', to: 'cookies#reset_cookies'
-  get '/delete-cookies', to: 'cookies#delete_cookies'
+  get '/set-cookies', to: 'cookies#create'
+  get '/reset-cookies', to: 'cookies#reset'
+  get '/delete-cookies', to: 'cookies#destroy'
   get '/show-cookies', to: 'cookies#show'
 end
